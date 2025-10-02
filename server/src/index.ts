ef9aebd23cfbed40804939e46d42fcfcdc1f1222
pyakz <markvergelbanguis@gmail.com>
2025-10-02 15:34:40 +0800

    qwe

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,4 @@
 import { Hono } from "hono";
-import { cors } from "hono/cors";
 import { serveStatic } from "hono/bun";
 import { logger } from "hono/logger";
 import { trimTrailingSlash } from "hono/trailing-slash";
