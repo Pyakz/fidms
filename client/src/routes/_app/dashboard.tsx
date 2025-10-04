b317f5913fb8668cb94aba00e814dab1f70c833a
pyakz <markvergelbanguis@gmail.com>
2025-10-04 22:03:41 +0800

    wewew

--- client/src/routes/_app/dashboard.tsx
+++ client/src/routes/_app/dashboard.tsx
@@ -1,5 +1,5 @@
 import { getLastUsedLoginMethod, useSession } from "@/lib/auth";
-import { Center, Text } from "@mantine/core";
+import { Center, Paper, Text } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_app/dashboard")({
@@ -21,13 +21,14 @@ function RouteComponent() {
     return <div>Access Denied</div>;
   }
   const lastMethod = getLastUsedLoginMethod();
-  console.log(lastMethod); // "google", "email", "github", etc.
   return (
     <Center className="h-screen">
-      <Text>Last login method: {lastMethod ?? "unknown"}</Text>
-      <pre>
-        <code>{JSON.stringify(data?.user, null, 2)}</code>
-      </pre>
+      <Paper withBorder shadow="md" p="md" mt="md">
+        <Text>Last login method: {lastMethod ?? "unknown"}</Text>
+        <pre>
+          <code>{JSON.stringify(data?.user, null, 2)}</code>
+        </pre>
+      </Paper>
     </Center>
   );
 }
